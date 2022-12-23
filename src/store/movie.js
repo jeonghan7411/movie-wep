ec204c273ea2a4e197ba86da0d892fbf2e1db6d5
jeonghan7411 <aufkrh@naver.com>
2022-12-24 01:07:21 +0900

    Add Serverless Functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -19,9 +19,16 @@ export const searchMovies = async (page) => {
     store.state.message = "";
   }
   try {
-    const res = await fetch(
-      `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
-    );
+    // const res = await fetch(
+    //   `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
+    // );  api/movie.js 파일에 만들어 둔걸 가져오는거임 api키를 서버에 숨긴것
+    const res = await fetch("/api/movie", {
+      method: "POST",
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page,
+      }),
+    });
     const { Search, totalResults, Response, Error } = await res.json();
     if (Response === "True") {
       store.state.movies = [...store.state.movies, ...Search];
@@ -38,9 +45,12 @@ export const searchMovies = async (page) => {
 
 export const getMovieDetails = async (id) => {
   try {
-    const res = await fetch(
-      `https://omdbapi.com?apikey=7035c60c&i=${id}&plot=full`
-    );
+    const res = await fetch("/api/movie", {
+      method: "POST",
+      body: JSON.stringify({
+        id,
+      }),
+    });
     store.state.movie = await res.json();
   } catch (err) {
     console.log("getMovieDetails error:", err);
